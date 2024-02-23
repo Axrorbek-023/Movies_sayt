06bf3216336e856b2caddd1cbe9015fbca4f027d
Axrorbek-023 <axrorbek2231@gmail.com>
2024-02-24 00:44:44 +0500

    Movies2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-let partelMovies = movies.slice(100,200)
+let partMovies = movies.slice(100,200)
 let elMovList = document.querySelector('.movises__list')
 let elOption = document.querySelectorAll('option')
+let elSelCategory = document.querySelector('.sel__category')
 
-partelMovies.forEach((item, index)=>{
+fnRender(partMovies)
+
+function fnRender(data){
+  elMovList.innerHTML = ''
+  data.forEach((item, index)=>{
     let newLi = document.createElement('li')
-    let val = elOption.value
     newLi.classList = 'movies__item'
     newLi.innerHTML = `
     <div class="card" style="width: 18rem;">
@@ -12,6 +16,8 @@ partelMovies.forEach((item, index)=>{
                     <div class="card-body">
                       <h5 class="card-title">${item.Title.toString().slice(0,20)}</h5>
                       <p class="card-text">${item.Categories}</p>
+                      <p class="card-text">${item.movie_year}</p>
+                      <h2 class="card-text">${item.imdb_rating}</h2>
                       <a href="https://www.youtube.com/watch?v=${item.ytid}" target="_blank" class="btn btn-primary">watch movie</a>
                     </div>
                   </div>
@@ -19,3 +25,41 @@ partelMovies.forEach((item, index)=>{
     elMovList.appendChild(newLi)
     
 });
+}
+
+
+function fnYear(value){
+  if(value == 'old'){
+    fnRender(partMovies.sort((a,b)=> a.movie_year - b.movie_year));
+  }else{
+    fnRender(partMovies.sort((a,b)=> b.movie_year - a.movie_year));
+  }
+}
+
+
+function fnRenting(value){
+  if(value == 'min'){
+    fnRender(partMovies.sort((a,b)=> a.imdb_rating - b.imdb_rating));
+  }else{
+    fnRender(partMovies.sort((a,b)=> b.imdb_rating - a.imdb_rating));
+  }
+}
+
+let arrCategory = []
+partMovies.forEach((item)=>{
+  if(!arrCategory.includes(item.Categories)){
+    arrCategory.push(item.Categories)
+  }
+})
+
+arrCategory.forEach(item=>{
+  let newOption = document.createElement('option')
+  newOption.textContent = item
+  newOption.value = item
+  elSelCategory.appendChild(newOption)
+})
+
+function fnCategory(value){
+  console.log(value);
+  fnRender(partMovies.filter((item)=> item.Categories == value));
+}
